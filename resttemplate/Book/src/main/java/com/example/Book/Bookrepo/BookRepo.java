5c67b4c1edbf4375a377ed0e9adb027c4f6078d4
GomathiS54 <lisha05mathi@gmail.com>
2024-08-07 17:15:16 +0530

    2nd file add

--- resttemplate/Book/src/main/java/com/example/Book/Bookrepo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Book.Bookrepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Book.BookDto.BookDto;
-
-public interface BookRepo extends JpaRepository<BookDto, Integer>{
-
-}
